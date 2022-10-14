4f10e28e01bcd23464179df3f4c0fe004afffe92
Shrenik Jain <shrenikkjain81@gmail.com>
2022-10-15 01:23:43 +0530

    reverted changes

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def image_upload():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host = '0.0.0.0', port = port, debug = True, ssl_context='adhoc')
+    app.run(host = '0.0.0.0', port = port, debug = True)
